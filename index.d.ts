23f8f2eab2a1a38aefd668be7b5efd141211420f
Ilya Markevich <ilya.markevich1991@gmail.com>
2025-12-17 00:29:55 +0100

    add typescript test

--- index.d.ts
+++ index.d.ts
@@ -3,85 +3,79 @@ declare class Validator {
 
   public getValidationObject(): object;
 
-  public property(path: string): Validator.State;
+  public property(path: string): State;
 
   public hasErrors(): Promise<boolean>;
 
-  public getErrors(): Promise<Validator.ValidationError[]>;
+  public getErrors(): Promise<ValidationError[]>;
 
-  public extend(validators: Validator.ValidatorsObject): void;
+  public extend(validators: ValidatorsObject): void;
 }
 
-declare namespace Validator {
-  export interface ValidationError {
-    path: string;
-    value: any;
-    errorMessage: string | null;
-  }
-
-  export interface ValidatorsObject {
-    [propName: string]: FieldValidator;
-  }
-
-  export interface State {
-    optional(): State;
-
-    withMessage(errorMessage: string): State;
-
-    isArray(): State;
-
-    isArrayLength(options?: { min?: number; max?: number }): State;
-
-    isBase64String(): State;
-
-    isBoolean(options?: { convert?: boolean }): State;
-
-    isDate(options?: { before?: Date | string; after?: Date | string }): State;
-
-    isEachIn(inArray: any[]): State;
-
-    isEmail(): State;
-
-    isEqual(equalTo: any): State;
-
-    isFloat(options?: { min?: number; max?: number; convert?: boolean }): State;
-
-    isIn(inArray: any[]): State;
-
-    isInteger(options?: {
-      min?: number;
-      max?: number;
-      convert?: boolean;
-    }): State;
-
-    isIpString(options?: { v4?: boolean; v6?: boolean }): State;
-
-    isJsonString(): State;
-
-    isLength(options?: { min?: number; max?: number }): State;
-
-    isLowerCaseString(): State;
-
-    isMatch(matchRegexp: RegExp): State;
-
-    isNotEmpty(): State;
-
-    isNumericString(): State;
-
-    isString(): State;
-
-    isUpperCaseString(): State;
-
-    isUrlString(): State;
-
-    [propName: string]: (options?: any) => State;
-  }
-
-  interface FieldValidator {
-    execute: (value: any, opts?: any) => boolean | Promise<boolean>;
-    defaultOpts?: object;
-    getErrorMessage?: (opts?: any) => string;
-  }
+export interface ValidationError {
+  path: string;
+  value: unknown;
+  errorMessage: string | null;
 }
 
-export = Validator;
+export interface ValidatorsObject {
+  [propName: string]: FieldValidator;
+}
+
+interface State {
+  optional(): State;
+
+  withMessage(errorMessage: string): State;
+
+  isArray(): State;
+
+  isArrayLength(options?: { min?: number; max?: number }): State;
+
+  isBase64String(): State;
+
+  isBoolean(options?: { convert?: boolean }): State;
+
+  isDate(options?: { before?: Date | string; after?: Date | string }): State;
+
+  isEachIn(inArray: unknown[]): State;
+
+  isEmail(): State;
+
+  isEqual(equalTo: unknown): State;
+
+  isFloat(options?: { min?: number; max?: number; convert?: boolean }): State;
+
+  isIn(inArray: unknown[]): State;
+
+  isInteger(options?: { min?: number; max?: number; convert?: boolean }): State;
+
+  isIpString(options?: { v4?: boolean; v6?: boolean }): State;
+
+  isJsonString(): State;
+
+  isLength(options?: { min?: number; max?: number }): State;
+
+  isLowerCaseString(): State;
+
+  isMatch(matchRegexp: RegExp): State;
+
+  isNotEmpty(): State;
+
+  isNumericString(): State;
+
+  isString(): State;
+
+  isUpperCaseString(): State;
+
+  isUrlString(): State;
+
+  [propName: string]: (options?: unknown) => State;
+}
+
+export interface FieldValidator {
+  execute: (value: unknown, opts?: unknown) => boolean | Promise<boolean>;
+  defaultOpts?: object;
+  getErrorMessage?: (opts?: unknown) => string;
+}
+
+export default Validator;
--- tsTests/fieldValidators.ts
+++ tsTests/fieldValidators.ts
@@ -1,64 +1,85 @@
-import Validator = require('../index');
+import Validator from '../index';
 
-function isArray(validator: Validator): void {
+const isArray = (validator: Validator) => {
   validator.property('prop1').isArray().withMessage('test');
-}
+};
 
-function isArrayLength(validator: Validator): void {
+const isArrayLength = (validator: Validator) => {
   validator.property('prop1').isArrayLength().withMessage('test');
 
-  validator.property('prop2').isArrayLength({
-    min: 1
-  }).withMessage('test');
+  validator
+    .property('prop2')
+    .isArrayLength({
+      min: 1
+    })
+    .withMessage('test');
 
-  validator.property('prop3').isArrayLength({
-    min: 1,
-    max: 2
-  }).withMessage('test');
+  validator
+    .property('prop3')
+    .isArrayLength({
+      min: 1,
+      max: 2
+    })
+    .withMessage('test');
 
-  validator.property('prop4').isArrayLength({
-    max: 2
-  }).withMessage('test');
-}
+  validator
+    .property('prop4')
+    .isArrayLength({
+      max: 2
+    })
+    .withMessage('test');
+};
 
-function isBase64String(validator: Validator): void {
+const isBase64String = (validator: Validator) => {
   validator.property('prop1').isBase64String().optional();
-}
+};
 
-function isBoolean(validator: Validator): void {
+const isBoolean = (validator: Validator) => {
   validator.property('prop1').isBoolean().optional();
 
-  validator.property('prop2').isBoolean({
-    convert: true
-  }).optional();
-}
+  validator
+    .property('prop2')
+    .isBoolean({
+      convert: true
+    })
+    .optional();
+};
 
-function isDate(validator: Validator): void {
+const isDate = (validator: Validator) => {
   validator.property('prop1').isDate().optional();
 
-  validator.property('prop2').isDate({
-    before: new Date()
-  }).optional();
+  validator
+    .property('prop2')
+    .isDate({
+      before: new Date()
+    })
+    .optional();
 
-  validator.property('prop3').isDate({
-    after: new Date()
-  }).optional();
+  validator
+    .property('prop3')
+    .isDate({
+      after: new Date()
+    })
+    .optional();
 
-  validator.property('prop4').isDate({
-    before: new Date(),
-    after: new Date()
-  }).optional();
-}
+  validator
+    .property('prop4')
+    .isDate({
+      before: new Date(),
+      after: new Date()
+    })
+    .optional();
+};
 
-function isEachIn(validator: Validator): void {
+const isEachIn = (validator: Validator) => {
   validator.property('prop1').isEachIn(['test']).optional();
-}
+};
 
-function isEmail(validator: Validator): void {
+const isEmail = (validator: Validator) => {
   validator.property('prop1').isEmail().optional();
-}
+};
 
-function isEqual(validator: Validator): void {
+const isEqual = (validator: Validator) => {
   validator.property('prop1').isEqual('test').optional();
 
   validator.property('prop2').isEqual(null).optional();
@@ -66,127 +87,167 @@ function isEqual(validator: Validator): void {
   validator.property('prop3').isEqual(0).optional();
 
   validator.property('prop4').isEqual(undefined).optional();
-}
+};
 
-function isFloat(validator: Validator): void {
+const isFloat = (validator: Validator) => {
   validator.property('prop1').isFloat().optional();
 
-  validator.property('prop2').isFloat({
-    convert: true
-  }).optional();
+  validator
+    .property('prop2')
+    .isFloat({
+      convert: true
+    })
+    .optional();
 
-  validator.property('prop3').isFloat({
-    min: 0
-  }).optional();
+  validator
+    .property('prop3')
+    .isFloat({
+      min: 0
+    })
+    .optional();
 
-  validator.property('prop4').isFloat({
-    max: 0
-  }).optional();
+  validator
+    .property('prop4')
+    .isFloat({
+      max: 0
+    })
+    .optional();
 
-  validator.property('prop5').isFloat({
-    min: 0,
-    max: 0,
-    convert: true
-  }).optional();
-}
+  validator
+    .property('prop5')
+    .isFloat({
+      min: 0,
+      max: 0,
+      convert: true
+    })
+    .optional();
+};
 
-function isIn(validator: Validator): void {
+const isIn = (validator: Validator) => {
   validator.property('prop1').isIn(['test']).optional();
-}
+};
 
-function isInteger(validator: Validator): void {
+const isInteger = (validator: Validator) => {
   validator.property('prop1').isInteger().optional();
 
-  validator.property('prop2').isInteger({
-    convert: true
-  }).optional();
+  validator
+    .property('prop2')
+    .isInteger({
+      convert: true
+    })
+    .optional();
 
-  validator.property('prop3').isInteger({
-    min: 0
-  }).optional();
+  validator
+    .property('prop3')
+    .isInteger({
+      min: 0
+    })
+    .optional();
 
-  validator.property('prop4').isInteger({
-    max: 0
-  }).optional();
+  validator
+    .property('prop4')
+    .isInteger({
+      max: 0
+    })
+    .optional();
 
-  validator.property('prop5').isInteger({
-    min: 0,
-    max: 1,
-    convert: true
-  }).optional();
-}
+  validator
+    .property('prop5')
+    .isInteger({
+      min: 0,
+      max: 1,
+      convert: true
+    })
+    .optional();
+};
 
-function isIpString(validator: Validator): void {
+const isIpString = (validator: Validator) => {
   validator.property('prop1').isIpString().optional();
 
-  validator.property('prop2').isIpString({
-    v4: true
-  }).optional();
+  validator
+    .property('prop2')
+    .isIpString({
+      v4: true
+    })
+    .optional();
 
-  validator.property('prop3').isIpString({
-    v6: true
-  }).optional();
+  validator
+    .property('prop3')
+    .isIpString({
+      v6: true
+    })
+    .optional();
 
-  validator.property('prop4').isIpString({
-    v4: true,
-    v6: true
-  }).optional();
-}
+  validator
+    .property('prop4')
+    .isIpString({
+      v4: true,
+      v6: true
+    })
+    .optional();
+};
 
-function isJsonString(validator: Validator): void {
+const isJsonString = (validator: Validator) => {
   validator.property('prop1').isJsonString().optional();
-}
+};
 
-function isLength(validator: Validator): void {
+const isLength = (validator: Validator) => {
   validator.property('prop1').isLength().optional();
 
-  validator.property('prop2').isLength({
-    min: 0
-  }).optional();
+  validator
+    .property('prop2')
+    .isLength({
+      min: 0
+    })
+    .optional();
 
-  validator.property('prop3').isLength({
-    max: 0
-  }).optional();
+  validator
+    .property('prop3')
+    .isLength({
+      max: 0
+    })
+    .optional();
 
-  validator.property('prop4').isLength({
-    min: 0,
-    max: 1
-  }).optional();
-}
+  validator
+    .property('prop4')
+    .isLength({
+      min: 0,
+      max: 1
+    })
+    .optional();
+};
 
-function isLowerCaseString(validator: Validator): void {
+const isLowerCaseString = (validator: Validator) => {
   validator.property('prop1').isLowerCaseString().optional();
-}
+};
 
-function isMatch(validator: Validator): void {
-  validator.property('prop1').isMatch(/test/).optional();
+const isMatch = (validator: Validator) => {
+  validator.property('prop1').isMatch(/test/u).optional();
 
-  validator.property('prop2').isMatch(new RegExp('test')).optional();
-}
+  validator.property('prop2').isMatch(/test/gu).optional();
+};
 
-function isNotEmpty(validator: Validator): void {
+const isNotEmpty = (validator: Validator) => {
   validator.property('prop1').isNotEmpty().optional();
-}
+};
 
-function isNumericString(validator: Validator): void {
+const isNumericString = (validator: Validator) => {
   validator.property('prop1').isNumericString().optional();
-}
+};
 
-function isString(validator: Validator): void {
+const isString = (validator: Validator) => {
   validator.property('prop1').isString().optional();
-}
+};
 
-function isUpperCaseString(validator: Validator): void {
+const isUpperCaseString = (validator: Validator) => {
   validator.property('prop1').isUpperCaseString().optional();
-}
+};
 
-function isUrlString(validator: Validator): void {
+const isUrlString = (validator: Validator) => {
   validator.property('prop2').isUrlString().optional();
-}
+};
 
-const validator: Validator = new Validator({
-  test: 1
-});
+const validator: Validator = new Validator({ test: 1 });
 
 isArray(validator);
 isArrayLength(validator);
@@ -209,6 +270,3 @@ isNumericString(validator);
 isString(validator);
 isUpperCaseString(validator);
 isUrlString(validator);
-
-
-
